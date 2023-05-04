7be7f49312b9e613b54a1e61c217c7d422cfc72b
HaleySchuhl <haleyschuhl@gmail.com>
2023-05-04 18:13:04 -0500

    move _find_closest_point into classes.py to get used directly

--- plantcv/plantcv/classes.py
+++ plantcv/plantcv/classes.py
@@ -6,9 +6,26 @@ import numpy as np
 from plantcv.plantcv import fatal_error
 import matplotlib.pyplot as plt
 from math import floor
-from plantcv.plantcv.annotate.points import _find_closest_pt
+from scipy.spatial import distance
 
+# Class helpers 
+def _find_closest_pt(pt, pts):
+    """ Given coordinates of a point and a list of coordinates of a bunch of points, find the point that has the
+    smallest Euclidean to the given point
 
+    :param pt: (tuple) coordinates of a point
+    :param pts: (a list of tuples) coordinates of a list of points
+    :return: index of the closest point and the coordinates of that point
+    """
+    if pt in pts:
+        idx = pts.index(pt)
+        return idx, pt
+
+    dists = distance.cdist([pt], pts, 'euclidean')
+    idx = np.argmin(dists)
+    return idx, pts[idx]
+
+# Class definitions
 class Params:
     """PlantCV parameters class."""
 
